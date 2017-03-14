492ac8de885d5cc24bda9f593e685089bb03bd10
gaoziqiang <gaoziqianghi@.com>
2017-03-14 18:28:34 +0800

    Connection Controller

--- src/main/java/cn/edu/sdut/softlab/controller/ConnectionController.java
+++ src/main/java/cn/edu/sdut/softlab/controller/ConnectionController.java
@@ -1,17 +1,30 @@
 package cn.edu.sdut.softlab.controller;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import cn.edu.sdut.softlab.connectioninterface.Connection;
+import cn.edu.sdut.sotflab.qualifier.MyConnection;
+import cn.edu.sdut.sotflab.qualifier.Simple;
 
 @Named("controller")
+@RequestScoped
 public class ConnectionController {
 
 	@Inject
-	private Connection connection;
+	@MyConnection
+	Connection connection;
 
 	public Connection getConnection() {
 		return connection;
+	}
+	
+	public String doSomething() {
+		
+		//connection.connect();
+		String connectMsg = "Connection has connnected!";
+		System.out.println(connectMsg);
+		return connectMsg;
 	}
 }
